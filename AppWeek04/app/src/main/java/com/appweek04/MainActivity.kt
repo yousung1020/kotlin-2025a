c6ddcbd118cbdef9749de1bfc7eba0a024744583
yousung1020 <choiyousung1020@gmail.com>
2025-09-26 11:29:07 +0900

    week04: Greeting App v0.1

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.appweek04
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // @+id 에 있는 변수를 가져옴
+        val editTextName = findViewById<EditText>(R.id.editTextName)
+        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
+        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+
+        // 버튼을 눌렀을 때
+        buttonGreet.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+
+            var greeting = ""
+            if(name.isNotEmpty()){
+                greeting = "안녕하세요, ${name}님!!!"
+            }
+            else{
+                greeting = "이름을 입력하세요"
+            }
+            textViewGreeting.text = greeting
+            textViewGreeting.visibility = View.VISIBLE
+            Log.d("KotlinWeek04App", greeting)
         }
     }
 }
