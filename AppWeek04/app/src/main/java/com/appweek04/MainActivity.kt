9fad922eec6283f8a08321afc974680700a12107
yousung1020 <choiyousung1020@gmail.com>
2025-09-26 12:12:06 +0900

    week04: Color Changer App v0.1

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.appweek04
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,25 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // @+id 에 있는 변수를 가져옴
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
-        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+        val buttonColor = findViewById<Button>(R.id.buttonColor)
 
-        // 버튼을 눌렀을 때
-        buttonGreet.setOnClickListener {
-            val name = editTextName.text.toString().trim()
-
-            var greeting = ""
-            if(name.isNotEmpty()){
-                greeting = "안녕하세요, ${name}님!!!"
-            }
-            else{
-                greeting = "이름을 입력하세요"
-            }
-            textViewGreeting.text = greeting
-            textViewGreeting.visibility = View.VISIBLE
-            Log.d("KotlinWeek04App", greeting)
+        buttonColor.setOnClickListener {
+            startActivity(Intent(this, ColorActivity::class.java))
         }
+
+        // @+id 에 있는 변수를 가져옴
+//        val editTextName = findViewById<EditText>(R.id.editTextName)
+//        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
+//        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+//
+//        // 버튼을 눌렀을 때
+//        buttonGreet.setOnClickListener {
+//            val name = editTextName.text.toString().trim()
+//
+//            var greeting = ""
+//            if(name.isNotEmpty()){
+//                greeting = "안녕하세요, ${name}님!!!"
+//            }
+//            else{
+//                greeting = "이름을 입력하세요"
+//            }
+//            textViewGreeting.text = greeting
+//            textViewGreeting.visibility = View.VISIBLE
+//            Log.d("KotlinWeek04App", greeting)
+//        }
     }
 }
