8f203c0545afbdd063a71dc5d816dfb7491c3f93
yousung1020 <choiyousung1020@gmail.com>
2025-11-07 10:43:53 +0900

    Student List App v0.3(ãRecyclerView)

---
+++ AppWeek09/app/src/main/java/com/AppWeek09/data/Student.kt
@@ -0,0 +1,15 @@
+package com.AppWeek09.data
+
+import java.util.Date
+import java.util.UUID
+
+class Student(
+    val name: String,
+    val id: String = UUID.randomUUID().toString(),
+    val department: String = "Computer Science",
+    val grade: String = "2nd year",
+    val email: String = "",
+    val addedDate: Date = Date()
+){
+    override fun toString(): String = name
+}
